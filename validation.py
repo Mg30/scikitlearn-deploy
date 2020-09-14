1a7c884e7ee023b9fcbab65f945155e60b42528e
matt <matgonzalez736@gmail.com>
2020-09-14 10:55:02 +0200

    add comment to validation

--- validation.py
+++ validation.py
@@ -2,6 +2,12 @@ from pydantic import BaseModel
 
 
 class PredictionData(BaseModel):
+    """ Validation class for FastApi
+     Attributs must match:
+     1- names of the columns of trained data
+     2- order of the columns of trained data
+    
+    """
     neighbourhood: str
     room_type: str
     minimum_nights: float
